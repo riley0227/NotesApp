64fc038f1b0e56b80d89c235b162b6734699b1f3
riley0227 <riley0227@gmail.com>
2022-05-22 08:48:25 -0700

    step-7

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -63,7 +63,8 @@ class _RegisterViewState extends State<RegisterView> {
                   email: email,
                   password: password,
                 );
-
+                final user = FirebaseAuth.instance.currentUser;
+                await user?.sendEmailVerification();
                 Navigator.of(context).pushNamed(verifyEmailRoute);
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'weak-password') {
