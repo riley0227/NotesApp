5be7ac1223e9d56b31b1a3c09452f254c43b973f
riley0227 <riley0227@gmail.com>
2022-06-02 12:07:06 -0700

    step-17

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:sparrow/constants/routes.dart';
 import 'package:sparrow/services/auth/auth_service.dart';
 import 'package:sparrow/views/login_view.dart';
-import 'package:sparrow/views/notes/new_note_view.dart';
+import 'package:sparrow/views/notes/create_update_note_view.dart';
 import 'package:sparrow/views/notes/notes_view.dart';
 import 'package:sparrow/views/register_view.dart';
 import 'package:sparrow/views/verify_email_view.dart';
@@ -21,7 +21,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
-        newNoteRoute: (context) => const NewNoteView(),
+        createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
       },
     ),
   );
