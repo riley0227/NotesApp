78591340f9ea91615cbc77bc9994c415f56fc378
riley0227 <riley0227@gmail.com>
2022-06-07 12:48:43 -0700

    step-21

---
+++ lib/utilities/dialogs/cannot_share_empty_note_dialog.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:sparrow/utilities/dialogs/generic_dialog.dart';
+
+Future<void> showCannotShareEmptyNoteDialog(BuildContext context) {
+  return showGenericDialog(
+    context: context,
+    title: 'Sharing',
+    content: 'You cannot share an empty note!',
+    optionBuilder: () => {
+      'OK': null,
+    },
+  );
+}
--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:sparrow/services/auth/auth_service.dart';
+import 'package:sparrow/utilities/dialogs/cannot_share_empty_note_dialog.dart';
 import 'package:sparrow/utilities/generics/get_arguments.dart';
 import 'package:sparrow/services/cloud/cloud_note.dart';
 import 'package:sparrow/services/cloud/cloud_storage_exceptions.dart';
@@ -92,6 +94,19 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('New Note'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final text = _textController.text;
+              if (_note == null || text.isEmpty) {
+                await showCannotShareEmptyNoteDialog(context);
+              } else {
+                Share.share(text);
+              }
+            },
+            icon: const Icon(Icons.share),
+          )
+        ],
       ),
       body: FutureBuilder(
         future: createOrGetExistingNote(context),
